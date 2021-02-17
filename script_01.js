1f9a621241835c98ead21097aedb7ed53a7ad9ab
Igor <igor.lischewski@web.de>
2021-02-17 08:30:32 +0100

    function_array

--- script_01.js
+++ script_01.js
@@ -10,7 +10,53 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-ausgabe(getSentence(["Ich","bin","Peter"],"S"));
-ausgabe(getSentence(["Bist","du","Peter"],"Q"));
-ausgabe(getSentence(["Ich","bin"],"E"));
-ausgabe(getSentence(["Ich","bin"],"Ekhkjhjk"));
+// ausgabe(getSentence("Ich","bin","Peter","S"));
+// ausgabe(getSentence(["Bist","du","Peter"],"Q"));
+// ausgabe(getSentence(["Ich","bin"],"E"));
+// ausgabe(getSentence(["Ich","bin"],"Ekhkjhjk"));
+
+let arr = ["Ich","bin","Peter"];
+ausgabe(getSentence(arr));
+
+function getSentence(op, S, Q, E) {
+    
+
+    switch (op) {
+        case ".":
+            return sentence(S);
+        case "?":
+            return question(Q);
+        case "!":
+            return exclamation(E);
+
+        default:
+            return "Irgendwas ging schief!";
+    }
+}
+
+function sentence(S){
+    return S;
+}
+
+
+function getSentenceArr(arr) {
+    let gap = " ";
+    let sentence = ".";
+    let question = "?";
+    let exclamation = "!";
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i != (arr.length-1)) {
+            str += arr[i] + gap;
+        } else if (let i = 0; i < arr.length; i++) {
+            str += arr[i] + sentence;
+        }  
+    }
+    return str;
+}
+
+
+function ausgabe(outputStr) {
+    console.log(outputStr);
+}
